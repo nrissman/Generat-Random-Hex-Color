6318217bace02f095658a159fcbb62a9ccfc87b2
nrissman <nrissman90@gmail.com>
2022-09-19 12:56:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const hex = document.querySelector(".hex")
+const btn = document.querySelector(".generate")
+
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+    document.body.style.backgroundColor = "#" + randomColor 
+    hex.innerHTML = "#" + randomColor
+}
+
+
+btn.addEventListener("click", generateColor)
+
+generateColor()
+
+// let color = Math.random()
+// color = Math.random().toString(16)
+
+// console.log(color)
